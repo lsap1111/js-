2597030e06eebe00c34091ffda4385e6c98285e1
Aayam Pawar <aayampawar@gmail.com>
2025-07-04 11:39:26 +0530

    01 variable

--- 01-variable.js
+++ 01-variable.js
@@ -1,1 +1,9 @@
-log
+accountType = "uganda"
+// accountName = 123; // This will throw an error because accountName is not declared with var, let, or const
+const accountBalance = 1000;
+let accountType = "savings"; 
+var accountStatus = "active";
+let state; 
+
+
+console.table([accountBalance, accountStatus, accountType, accountType, state]);
