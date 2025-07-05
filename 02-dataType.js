64cff6a33ec4956e8f9a85fdaf03373602ae45b9
Aayam Pawar <aayampawar@gmail.com>
2025-07-06 00:14:46 +0530

    data type

---
+++ 02-dataType.js
@@ -0,0 +1,59 @@
+"use strict"; // treat all JS code as newer version  
+ 
+// alert(3+3)  -we are using node js not the browser
+
+
+//*********************************data type**********************
+let name = "ap"
+let age = 18
+let isLoggingIn = false
+let state;
+
+// number => range- 2 to power 53
+// bigint;
+// string => ""
+// bullean => true/false
+// null => standalone value
+// undefined =>
+// object 
+// symboll  
+
+
+
+//   ****************************Conversion******************
+let score = "ap"
+//console.log(typeof score);
+
+let scoreBeNumber = Number(score)
+//console.log(scoreBeNumber);
+
+let scoreBeNumbe = Boolean(score);
+//console.log(scoreBeNumbe);
+
+
+
+//  *****************opreation***************************
+let numb = 45
+let negNumb = -numb
+//console.log(numb);
+
+//console.log(1+2);
+//console.log(1-2);
+//console.log(1/2); _divide
+//console.log(1%2); _reminder
+//console.log(1*2); _mul
+//console.log(1**2); _power 
+
+let stri1 = "ok"
+let st2 = "_pk"
+let str3 = stri1 + st2
+// console.log(str3);
+
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log(1 + 2 + "2");  // starting value are add here 
+//console.log("1" + 2 + 2);
+//console.log(1 + "2" + 2);
+
+
